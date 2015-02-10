54ef508e28fdaad8a714fcba716a616216ee8abb
mshchts <markoshchutskyy@gmail.com>
2015-02-10 20:22:01 +0200

    render _mainMenu in _header

--- vendor/assets/javascripts/jquery.datetimepicker/init.js
+++ vendor/assets/javascripts/jquery.datetimepicker/init.js
@@ -3,10 +3,5 @@ jQuery(document).on('ready page:change', function() {
     datetimefield.datetimepicker({
         format: 'Y-m-d H:i:s',
         lang: datetimefield.attr('lang') || datetimefield.parents('[lang]').attr('lang')
-	    minDate:'2010/01/01',
-	    maxDate:'2100/01/01',
-	    minTime:'08:00',
-	    maxTime:'12:01',
-	    step:15,
     });
 });
