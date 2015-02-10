643158582710dfe97088a128093e4b0d48ee3cf5
Artem Shymchuk <artem.shymchuk@gmail.com>
2015-02-10 12:10:36 +0200

    fix admin layout

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,10 +1,10 @@
 class Admin::ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  layout 'admin'
+
   protect_from_forgery with: :exception
 
   before_action :authenticate_admin!
-
-  layout 'admin.html.erb'
 
 end
