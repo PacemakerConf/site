045eba9c9db8b80ae685fc1a1d8a2fd16f10ad52
Igor <kotsiuba@hotmail.com>
2015-02-10 09:37:12 +0200

    try to connect cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(admin)
-  conferences_path 
+    conferences_path 
   end
+  
+  # redefine the current_ability method for CanCan.
+  def current_ability
+    @current_ability ||= Ability.new(current_admin)
+  end
+  
 end
