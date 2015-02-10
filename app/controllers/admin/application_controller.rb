b8d0318ae2a7da761ea14c7e2ab1b148b999bc6b
Artem Shymchuk <artem.shymchuk@gmail.com>
2015-02-10 10:50:26 +0200

    fix admin layout

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -3,7 +3,7 @@ class Admin::ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  layout 'admin'
+  layout 'admin.html.erb'
 
   def after_sign_in_path_for(admin)
     conferences_path
