21eaf485f32dcffe52ebba0e3e31ca098ba7b08c
Artem Shymchuk <artem.shymchuk@gmail.com>
2015-02-14 17:51:37 +0200

    change type of events duration field

--- db/migrate/20150214135410_add_position_and_duration_to_events.rb
+++ db/migrate/20150214135410_add_position_and_duration_to_events.rb
@@ -1,6 +1,6 @@
 class AddPositionAndDurationToEvents < ActiveRecord::Migration
   def change
-  	add_column :events, :duration, :integer
+  	add_column :events, :duration, :datetime
   	add_column :events, :position, :integer
   end
 end
