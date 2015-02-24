e9978e8343ea6592f5c54ac399037a4fdcd184c5
Artem Shymchuk <artem.shymchuk@gmail.com>
2015-02-24 10:59:46 +0200

    add eventtype test

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do 
 	factory :event do |f| 
-	    association :event_type, factory: :event_type
+	  association :event_type, factory: :event_type
 		f.title 'Beer-break'
 		f.conference_id 1
  	end
