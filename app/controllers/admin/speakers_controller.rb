04e282e825edf353fc018bf16a28fd8c4478cc0e
Mywe4ka <mywe4ka@gmail.com>
2015-02-24 23:00:07 +0200

    first generation link

--- app/controllers/admin/speakers_controller.rb
+++ app/controllers/admin/speakers_controller.rb
@@ -1,8 +1,6 @@
 class Admin::SpeakersController < Admin::ApplicationController
-  layout 'admin'
-  before_action :authenticate_admin!, except: [:new, :create, :index]
   before_action :set_speaker, only: [:show, :edit, :update, :destroy]
-  #load_and_authorize_resource
+  
   # GET /speakers
   # GET /speakers.json
   def index
@@ -16,7 +14,7 @@ class Admin::SpeakersController < Admin::ApplicationController
 
   # GET /speakers/new
   def new
-    # authorize! :create, Speaker
+    raise params[:hash].inspect
     @speaker = Speaker.new
   end
 
@@ -29,19 +27,17 @@ class Admin::SpeakersController < Admin::ApplicationController
 
   def send_invitation
     #render text: params[:email]
-    InviteMailer.speaker_invite(params[:email], params[:message]).deliver_later
-    redirect_to admin_speakers_path, notice: 'Invitation was successfully sent.'
+    Speaker.invite_speaker(params[:email], params[:message])
+    render text: "Invitation sent"
   end
 
   # POST /speakers
   # POST /speakers.json
   def create
-    # authorize! :create, Speaker
     @speaker = Speaker.new(speaker_params)
 
     respond_to do |format|
       if @speaker.save
-        current_user['role'] = User::SPEAKER
         format.html { redirect_to admin_speakers_path, notice: 'Speaker was successfully created.' }
         format.json { render :show, status: :created, location: @speaker }
       else
@@ -87,7 +83,7 @@ class Admin::SpeakersController < Admin::ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def speaker_params
-      params.require(:speaker).permit(:name, :surname, :photo, :position, :description, :email, :facebook, :linkedin, :site)
+      params.require(:speaker).permit(:name, :surname, :photo, :description, :email, :facebook, :linkedin, :site)
     end
 
 end
