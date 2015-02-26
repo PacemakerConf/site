7fd74d9e29d408570405a7909fd63f20dd354267
Mywe4ka <mywe4ka@gmail.com>
2015-02-27 00:55:35 +0200

    Added link, html view

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -22,24 +22,22 @@ Enjoy!
 	validates :surname, presence: true
 	validates :position, presence: true
 	validates :description, presence: true
-
 	validates_attachment_content_type :photo, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 	def fullname
 		[name, surname].join ' '
 	end
 
-	def self.generate_link(email, conf_id=1)
+	def self.generate_link(email)
 		cost = 10
-		hash = ::BCrypt::Password.create("#{email}_#{conf_id}", :cost => cost).to_s
+		hash = ::BCrypt::Password.create("#{email}", :cost => cost).to_s
 		LinkHelper.new.link_to('Create Your Profile', 
-			Rails.application.routes.url_helpers.new_speaker_path(hash: hash))
+			"http://localhost:3000/admin"+Rails.application.routes.url_helpers.new_speaker_path(hash: hash))
 	end	
 	def self.invite_speaker(email, message)
         # if params[:message].match(INVITE_MESSAGE_REGEXP)
 		# insert link Speaker.generate_link(email), email, hash into invirations
 	    message = message.gsub!(INVITE_MESSAGE_REGEXP, Speaker.generate_link(email))
 	    InviteMailer.speaker_invite(email, message).deliver_later
-
 	end	
 end
