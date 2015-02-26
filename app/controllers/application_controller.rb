6ca8f82121aae1734f7c3492f5be621dcc7e9683
Igor <kotsiuba@hotmail.com>
2015-02-26 17:38:45 +0200

    add some tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,12 @@ class ApplicationController < ActionController::Base
 
   def current_user
     current_admin.nil? ?  
-      session['current_user'] ||= {'role': User::GUEST}:
-      session['current_user'] = {'role': User::ADMIN}
+      session['current_user'] ||= {'role' => User::GUEST} :
+      session['current_user'] = {'role'=> User::ADMIN}
   end  
   
   # redefine the current_ability method for CanCan.
   def current_ability
-  #raise "current_admin: #{current_admin.inspect}, current_user: #{current_user.inspect}"
     @current_ability ||= Ability.new(current_admin || current_user) 
   end
  
