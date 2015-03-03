ae6db5ea91559ab6dabc17b6655eef6139148ed1
Igor <kotsiuba@hotmail.com>
2015-03-03 23:01:01 +0200

    some changes of tests

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,29 +2,21 @@ require 'rails_helper'
 require 'shoulda/matchers'
 
 describe Admin do
-
-  before do
-	  @admin = Admin.new(username: 'adminstrator', password: 'very_secret',
-						email: 'adminstrator@example.com')
-  end
-
+  
   it 'is valid with username, password and email' do
-    expect(@admin).to be_valid
+    expect(FactoryGirl.create(:admin)).to be_valid
   end
 
   it 'is invalid without an email address' do
-    @admin.email = nil
-    expect(@admin).not_to be_valid
+    expect(FactoryGirl.build(:admin, email: nil)).not_to be_valid
   end
 
   it 'is invalid without username' do
-    @admin.username = nil
-    expect(@admin).not_to be_valid
+    expect(FactoryGirl.build(:admin, username: nil)).not_to be_valid
   end
 
   it 'is invalid without password' do
-    @admin.password = nil
-    expect(@admin).not_to be_valid
+    expect(FactoryGirl.build(:admin, password: nil)).not_to be_valid
   end
 
 end
