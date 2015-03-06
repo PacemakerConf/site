97c48aeb99321e9623b365fb9f43283c5f0f113e
Artem Shymchuk <artem.shymchuk@gmail.com>
2015-03-06 12:17:03 +0200

    fix few shit

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -10,7 +10,7 @@ class Conference < ActiveRecord::Base
 	validates :name, presence: true
 	validates :year, presence: true,
 					 inclusion: { in: 2011..2100 }
-	validates_with Validators::ConferenceUniquenessValidator
+	# validates_with Validators::ConferenceUniquenessValidator
 					 
 	def fullname
 		name.to_s + "-" + year.to_s
@@ -22,7 +22,7 @@ class Conference < ActiveRecord::Base
 
 	def self.last_conference_route
 		if Conference && Conference.first
-			last_conference = Conference.order(date: :desc)[0]
+			last_conference = Conference.where(published: true).order(date: :desc)[0]
 			last_conference.name.to_s + '-' + last_conference.year.to_s + '/about'
 		else
 			'1'
