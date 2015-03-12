d13e1e95b43788b34dda060c9e54836d27d8f107
Mywe4ka <mywe4ka@gmail.com>
2015-03-12 22:50:47 +0200

    add Mandrill for sending email

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-    :address => "smtp.gmail.com",
+    :address => "smtp.mandrillapp.com",
     :port => "587",
     :domain => "http://localhost:3000",
     :user_name => "sysstemtest@gmail.com",
-    :password => "Windows7!",
+    :password => "qxbmmL4vpBgpV8740v0Rgg",
     :authentication => "plain",
     :enable_starttls_auto => true
 }
