4fff61f3e3cc1b8bdd747efbd232e8fadf6627fc
Mywe4ka <mywe4ka@gmail.com>
2015-03-15 17:02:50 +0200

    changed check for hash

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -10,11 +10,6 @@ class Invitation < ActiveRecord::Base
 
 	INVITE_MESSAGE_TOKEN = '$${link_invitation}' 
 	INVITE_MESSAGE_REGEXP = /\$\$\{\w+\}/
-# 	INVITE_MESSAGE = <<-MESSAGE
-# Hi! You are invited for registration to the conference at our site! 
-# Your link is #{INVITE_MESSAGE_TOKEN}
-# Enjoy!
-# 	MESSAGE
 
 	def self.generate_link(email, email_hash)
 		LinkHelper.new.link_to('Click here to create profile', 
@@ -26,6 +21,5 @@ class Invitation < ActiveRecord::Base
 	    message = message.gsub!(INVITE_MESSAGE_REGEXP, Invitation.generate_link(email, email_hash))
 	    InviteMailer.speaker_invite(email, message).deliver  
 	end	
-
 
 end
