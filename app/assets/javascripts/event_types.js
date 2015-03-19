ba8040b250ba8b4bcb2e9b0d6e3cef690c1d6641
Blackleaves <valdihor@gmail.com>
2015-03-19 23:54:04 +0200

    Modified styles and JS for Schedule and EventTypes

--- app/assets/javascripts/event_types.js
+++ app/assets/javascripts/event_types.js
@@ -1,50 +1,52 @@
-  function imageFromField(){
-      obj = document.getElementById('event_type_image');
-      if(obj.innerHTML){
-          imageToField(obj.innerHTML);
-      }
-      else{
-          imageToField('0');
-      }
-  }
+function imageFromField(){
+    obj = $('#event_type_image');
+    if(obj.html()){
+        imageToField(obj.html());
+    }
+    else{
+        imageToField('0');
+    }
+}
 
-  function imageToField(number) {
-    document.getElementById('event_type_image').innerHTML = number;
-    document.getElementById('event_type_image').value = number;
+function imageToField(number) {
+  $('#event_type_image').html(number);
+  $('#event_type_image').val(number);
 
-    classString = "btn btn-default btn-sm glyphicon glyphicon-";
-    classString += GLYPHSJS[number];
-    document.getElementById('DropDownMark').className = classString;
-      
-      spanObj = document.getElementsByClassName('ddHead');
-      spanObj = spanObj[0].getElementsByTagName('ul');
-      spanObj = spanObj[0].getElementsByTagName('li');
-      for (var i = spanObj.length - 1; i >= 0; i--) {
-          spanObj[i].style.border = "1px solid transparent";
-      };
-      spanObj[number].style.border = "1px solid #563d7c";
-  }
+  classString = "btn btn-default btn-sm glyphicon glyphicon-";
+  classString += GLYPHSJS[number];
+  obj = $('#DropDownMark');
+  document.getElementById('DropDownMark').className = classString;
+
+  spanObj = $('.ddHead');
+  spanObj = spanObj[0].getElementsByTagName('ul');
+  spanObj = spanObj[0].getElementsByTagName('li');
+
+  for (var i = spanObj.length - 1; i >= 0; i--) {
+    spanObj[i].className = 'non-selected';
+  };
+  spanObj[number].className = 'selected';
+}
 
 
 function getDefaultDuration(){
-  selected_event_type =  event_event_type_id.value;
-  xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var response = JSON.parse( xmlhttp.responseText );
-        if(response.durationHour < 10){
-          response.durationHour = '0' + response.durationHour 
-        }
-        if(response.durationMin < 10){
-          response.durationMin = '0' + response.durationMin 
-        }
-        event_duration_4i.value = response.durationHour;
-        event_duration_5i.value = response.durationMin;
-        
-        toggleEventFields( response.speakerEvent == 'true');
-    }
+selected_event_type =  event_event_type_id.value;
+xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      var response = JSON.parse( xmlhttp.responseText );
+      if(response.durationHour < 10){
+        response.durationHour = '0' + response.durationHour 
+      }
+      if(response.durationMin < 10){
+        response.durationMin = '0' + response.durationMin 
+      }
+      event_duration_4i.value = response.durationHour;
+      event_duration_5i.value = response.durationMin;
+      
+      toggleEventFields( response.speakerEvent == 'true');
   }
-  xmlhttp.open("GET", "/admin/event_types/" + selected_event_type + ".json", true);
-  xmlhttp.send();
+}
+xmlhttp.open("GET", "/admin/event_types/" + selected_event_type + ".json", true);
+xmlhttp.send();
 
 }
