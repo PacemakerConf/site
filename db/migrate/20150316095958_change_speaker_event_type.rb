1f8473ba41efeeab3858f762d03506197248a89d
Dmytro Panov <dmytro@Lenovo.(none)>
2015-03-19 05:01:00 +0200

    fix migration and check if column exists

--- db/migrate/20150316095958_change_speaker_event_type.rb
+++ db/migrate/20150316095958_change_speaker_event_type.rb
@@ -1,5 +1,23 @@
 class ChangeSpeakerEventType < ActiveRecord::Migration
-  def change
-  	change_column :event_types, :speakerEvent, 'boolean USING CAST(speakerEvent AS boolean)'
+  def up
+    if ActiveRecord::Base.connection.column_exists?(:event_types, :speakerEvent)
+      connection.execute(%q{
+        alter table event_types
+        alter column speakerEvent
+        type integer using cast(speakerEvent as boolean)
+    else
+      add_column :event_types, :speakerEvent, :boolean
+    end
+  end
+
+  def down
+    if ActiveRecord::Base.connection.column_exists?(:event_types, :speakerEvent)
+      connection.execute(%q{
+        alter table event_types
+        alter column speakerEvent
+        type integer using cast(speakerEvent as integer)
+    else
+      add_column :event_types, :speakerEvent, :integer
+    end
   end
 end
