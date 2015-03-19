71ecc02e7a38df3c4dc8d06d824e5bee203272d8
Igor <kotsiuba@hotmail.com>
2015-03-19 11:47:32 +0200

    small changes of Message model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -4,7 +4,6 @@ class Invitation < ActiveRecord::Base
 
 	validates :email, presence: true,
 		email_format: { message: "doesn't look like an email address" }
-	# validates_associated :message
 
 	delegate :content, to: :message, prefix: true
 	
