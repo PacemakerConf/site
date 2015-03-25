c7585aad3daadfe541f0f98e95f8d9c57eb455df
dpanov <dpanov@dpanov-VirtualBox.(none)>
2015-03-25 21:30:12 +0200

    add emails for some seeds, add checks for validator

--- lib/validators/event_speaker_validator.rb
+++ lib/validators/event_speaker_validator.rb
@@ -1,11 +1,11 @@
 module Validators
 	class EventSpeakerValidator < ActiveModel::Validator
 		def validate(record)
-			if( !record.event_type.speakerEvent && record.speaker_id ) 
+			if( record && record.event_type && !record.event_type.speakerEvent && record.speaker_id ) 
 				record.errors[:speaker_id] << 'error: you can\'t choose speaker to this eventtype'
 			end
 
-			if( record.event_type.speakerEvent && !record.speaker_id )
+			if( record && record.event_type && record.event_type.speakerEvent && !record.speaker_id )
 				record.errors[:speaker_id] <<  'error: you have to choose speaker to TOPIC or LIGHTNING event'
 			end
 		end
