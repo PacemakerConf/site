40f145de1284301d298162ca3fd8e50e1e739381
Bad1993 <hogdubno@gmail.com>
2015-03-27 00:58:48 +0200

    changes to client validation on contact form

--- Gemfile
+++ Gemfile
@@ -24,10 +24,10 @@ gem "paperclip-dropbox", ">= 1.1.7"
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
-group :assets do 
-  gem 'jquery-datatables-rails', github: 'rweng/jquery-datatables-rails'
-  gem 'jquery-ui-rails'
-end
+#group :assets do 
+  #gem 'jquery-datatables-rails', github: 'rweng/jquery-datatables-rails'
+  #gem 'jquery-ui-rails'
+#end
 gem 'will_paginate'
 gem 'momentjs-rails', '>= 2.8.1'
 gem 'twitter-bootstrap-rails'
--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -7,15 +7,19 @@ $(document).on("page:change", function(){
     var emailRegEx = /^\w+@\w+\.[a-z]+$/i;
     var isCorrect = true;
       $('.mail-adreess').each(function(index, obj){
-        if($(obj).val() != '' && $(obj).val().search(emailRegEx) != -1)
+        if($(obj).val() != '' && $(obj).val().search(emailRegEx) != -1){
           mails+=$(obj).val()+';';
-        else 
+           $(obj).parent().removeClass('has-error')
+        }
+        else {
           isCorrect = false;
+          $(obj).parent().addClass('has-error')
+        }
+        
       });
       if(isCorrect)
         $('[name="contact[email]"]').val(mails);
-      else
-        $('.mail-adreess').parent().addClass('has-error');
+
       return isCorrect;
     });
 });
