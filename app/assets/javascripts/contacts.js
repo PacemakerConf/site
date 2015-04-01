24e1aa76b5aa31e2a9c634a2b31dcf2ec0fa98ca
Bad1993 <hogdubno@gmail.com>
2015-04-01 22:16:09 +0300

    reseted changes

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -1,24 +1,16 @@
 $(document).on("page:change", function(){
 
-  $("#add_button").click(function() {
-    $("#mail-area").append(INPUT_DIV);
+    $("#add_button").click(function() {
+     var divHTML = 
+      '<div class="form-group ">'+
+       '<label class="col-lg-2 control-label label_contact">Email</label>'+
+        '<div class="col-lg-7 contact_form">'+
+          '<input class="form-control mail-adreess" type="text" >'+
+        '</div>'+
+        '<a class="remove_nested_fields btn-default btn-sm btn-danger" id="remove_button_contact" onclick="RemoveMail(this);return false;">-</a>'+
+      '</div>';
+      $("#mail-area").append(divHTML);
   });
-
-  var savedMails = $(".allmails").val();
-  var lastSymbol = savedMails.charAt(savedMails.length-1);
-    if(lastSymbol == ";")
-    {
-      savedMails = savedMails.substring(0, savedMails.length-1);
-    }
-  var mailsArray = savedMails.split(";");
-
-  if(mailsArray){
-    $.each(mailsArray,function(index, obj){
-        $("#mail-area").append(INPUT_DIV);
-        $(".mail-adreess:eq("+index+")").val(mailsArray[index]);
-      });
-      $(".mail-adreess:last").parents(':eq(1)').remove();
-  }
 
   $('[name="commit"]').click(function(){
     var mails = '';
@@ -34,7 +26,7 @@ $(document).on("page:change", function(){
           $(obj).parent().addClass('has-error')
 
           $("#error-area").html('');
-          $("#error-area").append(ERROR_DIV);
+          $("#error-area").append(divError);
         }
         
       });
@@ -42,22 +34,16 @@ $(document).on("page:change", function(){
         $('[name="contact[email]"]').val(mails);
         return isCorrect;
     });
+
+    
+
 });
 
-var ERROR_DIV =
-  '<div class="alert alert-dismissible alert-danger">'+
-    '<button type="button" class="close" data-dismiss="alert">×</button>'+
+var divError =
+'<div class="alert alert-dismissible alert-danger">'+
+      '<button type="button" class="close" data-dismiss="alert">×</button>'+
       '<strong>Oh snap!</strong>'+
-        '<ul>'+
+      '<ul>'+
           '<li>Email invalid or can\'t be blank</li>'+
-        '</ul>'+
-  '</div>';
-
-var INPUT_DIV = 
-  '<div class="form-group ">'+
-    '<label class="col-lg-2 control-label label_contact">Email</label>'+
-    '<div class="col-lg-7 contact_form">'+
-      '<input class="form-control mail-adreess" type="text" >'+
-    '</div>'+
-    '<a class="remove_nested_fields btn-default btn-sm btn-danger" id="remove_button_contact" onclick="RemoveMail(this);return false;">-</a>'+
-  '</div>';
+      '</ul>'+
+'</div>'
