96367c311baf6a5c3149dd6d365cf5a586dd3a6c
Artem Shymchuk <artem.shymchuk@gmail.com>
2015-04-01 18:35:01 +0300

    fix conf without date

--- app/assets/javascripts/mainMenu.js
+++ app/assets/javascripts/mainMenu.js
@@ -1,6 +1,5 @@
-$(document).on('ready page:load', setFlagColors);
-
 function setFlagColors(){
+	console.log('load');
 	if( typeof conference_menu != 'undefined'){
 		active_conference = $('#conference_menu').attr('active_conference')
 		active_year = $('#conference_menu').attr('active_year')
@@ -59,18 +58,22 @@ function setFlagColors(){
 
 		// Show all conference flags of current year on click and hide other flags
 		$('.flag-year-image-container').click(function(){
+			console.log('click');
 			var year = this.getAttribute('year'); 	
 			$('.year-box').addClass('unvisible');
 			$('.main-content').addClass('unvisible');
 			$('#year-' + year + '-box').removeClass('unvisible');
 
 			for(var i = 0; i < years.length; i++){
+				// console.log('year ' + years[i])
 				if(years[i] != year){
 					$('.conference-' + years[i]).hide();
+					console.log('hide ' + years[i]);
 					$('#flag-' + years[i]).children('img').attr('src', '/assets/greyflag.png');
 				}
 				else{
 					$('.conference-' + years[i]).fadeToggle();
+					console.log('toggle ' + years[i]);
 					$('#flag-' + years[i]).children('img').attr('src', '/assets/redflag.png');
 				}	
 			}
