9ff8004753dc3f88c99add75d2cbe4cfaa158e6d
Igor <kotsiuba@hotmail.com>
2015-04-03 00:13:18 +0300

    rename file

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(document).on('ready page:load', messagePageLoad);
 
 function messagePageLoad(){
-	if( $('.massage_data_table') ){
-		$('.massage_data_table').dataTable({
+	if( $('.message_data_table') ){
+		$('.message_data_table').dataTable({
      	paging: true,
 			dom: 'Rlfrtip',
 			"order": [[ 0, "desc" ]],
