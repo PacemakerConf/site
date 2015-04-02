c2d053a3ed6db0ac555eacd91c3d3fc1359921e6
Igor <kotsiuba@hotmail.com>
2015-04-03 00:02:38 +0300

    fix bug

--- app/assets/javascripts/massage.js
+++ app/assets/javascripts/massage.js
@@ -1,16 +1,12 @@
-$(document).on('ready page:load', pageLoad);
+$(document).on('ready page:load', messagePageLoad);
 
-function pageLoad(){
-
-	//datatable_massage
-		(function(){
-		if( $('.data_table_massage') ){
-			$('.data_table_massage').dataTable({
-       	paging: true,
-				dom: 'Rlfrtip',
-				"order": [[ 0, "desc" ]],
-				"aoColumnDefs": [{ "bSortable": false, "aTargets": [-1, -2] }]
-	    });
-  	}
-	})();
+function messagePageLoad(){
+	if( $('.massage_data_table') ){
+		$('.massage_data_table').dataTable({
+     	paging: true,
+			dom: 'Rlfrtip',
+			"order": [[ 0, "desc" ]],
+			"aoColumnDefs": [{ "bSortable": false, "aTargets": [-1, -2] }]
+    });
+	}
 }
